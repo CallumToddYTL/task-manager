5455cb46e04dab846cb66c2808c53b4b359ed75b
Callum <callum.todd@yandiyatechnologies.com>
2025-06-26 11:14:51 +0100

    Updated Registering
    Now the password must be strong. In order for a user to successfully create a password it need to: be 8 chars long, include capitalised letter, include lower cased letter, a digit, and a special char (&$%@, etc).

--- app/forms.py
+++ app/forms.py
@@ -2,10 +2,13 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, TextAreaField, SelectField
 from wtforms.validators import DataRequired, Length, EqualTo, ValidationError, Optional
 from .models import User
+import re
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=4, max=25)])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    password = PasswordField('Password', validators=[
+        DataRequired(), Length(min=8), 
+    ])
     confirm_password = PasswordField('Confirm Password', validators=[
         DataRequired(), EqualTo('password', message='Passwords must match')
     ])
@@ -16,6 +19,16 @@ class RegistrationForm(FlaskForm):
         existing_user = User.query.filter_by(username=username.data).first()
         if existing_user:
             raise ValidationError('This username is already taken.')
+
+    def validate_password(self, password):
+        pw = password.data
+        if (len(pw) < 8 or not re.search(r'[A-Z]', pw) or 
+            not re.search(r'[a-z]', pw) or not re.search(r'\d', pw) or 
+            not re.search(r'[^\w\s]', pw)):
+            raise ValidationError(
+                "Password must be at least 8 characters long and include at least one uppercase letter, "
+                "one lowercase letter, one digit, and one special character."
+            )   
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
